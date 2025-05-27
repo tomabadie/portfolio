5fcec7a884514e97d8bf856b9e7e9146df4324aa
tomabadie <abadie.thomas@gmail.com>
2025-05-27 23:39:28 +0200

    Improve mobile layout : adjust main padding and footer display

--- src/shared/components/Footer/Footer.tsx
+++ src/shared/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   const rights = language === 'en' ? 'All rights reserved.' : 'Tous droits réservés.';
 
   return (
-    <footer className="bg-global-primary border-primary flex items-center justify-end border-t px-4 py-2">
+    <footer className="bg-global-primary border-primary flex items-center justify-center border-t px-4 py-2 sm:justify-end">
       <p className="text-primary text-sm">
         {' '}
         &copy; {new Date().getFullYear()} Thomas Abadie. {rights}
