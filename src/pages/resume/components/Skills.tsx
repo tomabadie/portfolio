d48378d5517366853a0ab10fe9ba4e60dd2e88d9
tomabadie <abadie.thomas@gmail.com>
2025-06-02 23:09:02 +0200

    Done skills bars + display

--- src/pages/resume/components/Skills.tsx
+++ src/pages/resume/components/Skills.tsx
@@ -11,6 +11,25 @@ const Skills = () => {
       <h3 className="border-b-orange-accent-light dark:border-b-orange-accent-dark mb-2 w-fit border-b-2 font-bold">
         {language === 'en' ? 'Skills' : 'Compétences'}
       </h3>
+      <ul className="flex flex-col">
+        {skillsList.map((skill) => {
+          return (
+            <li key={skill.id} className="py-2">
+              <h4 className="w-fit font-bold">{skill.label}</h4>
+              {/* 
+              To use later : rank by category
+              <span className="border border-purple-500">{skill.category}</span>
+              */}
+              <div className="border-orange-accent-light dark:border-orange-accent-dark h-2.5 w-full rounded-full border p-0.5">
+                <div
+                  className="bg-orange-accent-light dark:bg-orange-accent-dark mr-auto h-full rounded-full"
+                  style={{ width: `${skill.level}%` }}
+                />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
