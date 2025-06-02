82a556f1f96a8799b9d4c57638335ed097af5c9e
tomabadie <abadie.thomas@gmail.com>
2025-06-03 00:34:55 +0200

    Done Experiences component - No Dialog

--- src/pages/resume/components/WorkExperiences.tsx
+++ src/pages/resume/components/WorkExperiences.tsx
@@ -11,6 +11,28 @@ const WorkExperiences = () => {
       <h3 className="border-b-accent-light dark:border-b-accent-dark transition-theme mb-2 w-fit border-b-2 font-bold">
         {language === 'en' ? 'Work experiences' : 'Expériences'}
       </h3>
+      <ul className="before:bg-border-primary-light dark:before:bg-border-primary-dark transition-theme relative space-y-8 pl-1 before:absolute before:-ml-px before:h-full before:w-0.5 before:rounded-full">
+        {experiencesList.map((item) => {
+          const startYear = item.startDate.split('/')[1];
+          const endYear = !item.endDate ? startYear : item.endDate.split('/')[1];
+
+          return (
+            <li key={item.id} className="relative -ms-1.5 flex items-start gap-4">
+              <span className="border-accent-light dark:border-accent-dark bg-global-secondary transition-theme size-3 shrink-0 rounded-full border-2" />
+
+              <div className="-mt-2">
+                <time className="text-secondary text-xs/none font-medium">
+                  {startYear === endYear ? endYear : `${startYear} – ${endYear}`} - {item.duration}
+                </time>
+
+                <h3 className="text-lg font-bold">{item.company}</h3>
+
+                <p className="text-secondary mt-0.5 text-sm">{item.position}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
