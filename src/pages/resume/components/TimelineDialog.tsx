a0e3bbbf90ad0c8f50f7d28d514227301d782af7
tomabadie <abadie.thomas@gmail.com>
2025-06-05 11:29:14 +0200

    Done responsive TimelineDialog

--- src/pages/resume/components/TimelineDialog.tsx
+++ src/pages/resume/components/TimelineDialog.tsx
@@ -24,14 +24,18 @@ const TimelineDialog = ({ isOpen, setIsOpen, focusedItem }: TimelineDialogProps)
   return (
     <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
       <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-        <DialogPanel className="border-primary bg-global-secondary mr-[calc(theme(spacing.4)-env(safe-area-inset-right))] max-h-[80dvh] max-w-4xl space-y-4 overflow-y-auto rounded-2xl border-3 p-12">
-          <div className="flex items-center justify-between">
-            <a href={focusedItem?.link} target="_blank" rel="noopener noreferrer">
+        <DialogPanel className="border-primary bg-global-secondary max-h-[80dvh] max-w-4xl space-y-4 overflow-y-auto rounded-2xl border-3 p-3 sm:p-8 lg:p-12">
+          <div className="flex flex-col gap-5 sm:flex-row sm:items-center sm:justify-between">
+            <a href={focusedItem?.link} target="_blank" rel="noopener noreferrer" className="w-fit">
               <DialogTitle className="text-primary transition-theme border-accent-light dark:border-accent-dark rounded-sm border-2 p-2 font-bold uppercase hover:shadow-[0_0_12px_4px_var(--color-btn-primary-hover-light)] dark:hover:shadow-[0_0_12px_4px_var(--color-btn-primary-hover-dark)]">
                 {focusedItem?.company}
               </DialogTitle>
             </a>
-            <button type="button" onClick={() => setIsOpen(false)}>
+            <button
+              className="order-first mx-auto sm:order-none sm:mx-0"
+              type="button"
+              onClick={() => setIsOpen(false)}
+            >
               <CloseIcon className="border-primary dark:stroke-primary-dark stroke-primary-light transition-theme h-10 w-10 cursor-pointer rounded-full border shadow-[0_0_12px_4px_var(--color-btn-primary-hover-light)] dark:shadow-[0_0_12px_4px_var(--color-btn-primary-hover-dark)]" />
             </button>
           </div>
