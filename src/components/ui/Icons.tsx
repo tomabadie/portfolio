3c94d5997c74b09061047cf60d555d8e55a81500
tomabadie <abadie.thomas@gmail.com>
2025-06-07 19:08:57 +0200

    Update accordion with style/link/animatedicon - WIP

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -133,3 +133,23 @@ export const StarIcon = ({ className }: StarIcon) => (
     />
   </svg>
 );
+
+interface ArrowRightIcon {
+  className?: string;
+}
+
+export const ArrowRightIcon = ({ className }: ArrowRightIcon) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={`${className} fill-none transition-transform`}
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+  >
+    <title>ArrowRight</title>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+    />
+  </svg>
+);
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import { Link } from 'react-router';
 import type { NavigationItemProps } from '../../components/layouts/Header/data/headerType';
-import { navDataEn } from '../../components/layouts/Header/data/navData.en';
-import { navDataFr } from '../../components/layouts/Header/data/navData.fr';
+
+import { navDataEn } from '../../components/navigation/data/navData.en';
+import { navDataFr } from '../../components/navigation/data/navData.fr';
+import { ArrowRightIcon } from '../../components/ui/Icons';
 import { useLanguage } from '../../contexts/LanguageContext';
 
 const Home = () => {
@@ -11,9 +14,9 @@ const Home = () => {
 
   const getSizes = (index: number): string => {
     const selectedIndex = selected ? navList.indexOf(selected) : -1;
-    if (index === selectedIndex) return 'basis-[50%]';
+    if (index === selectedIndex) return 'w-[55%]';
     /* if (Math.abs(index - selectedIndex) === 1) return 'basis-[20%]'; */
-    return 'basis-[10%]';
+    return 'w-[15%]';
   };
 
   return (
@@ -31,9 +34,9 @@ const Home = () => {
         {navList.map((navItem, index) => (
           <label
             key={navItem.id}
-            className={`flex h-full cursor-pointer items-center rounded-lg transition-all duration-300 ease-in-out ${getSizes(index)}`}
+            className={`flex h-full cursor-pointer items-center justify-around rounded-lg transition-all duration-300 ease-in-out ${getSizes(index)}`}
             style={{
-              backgroundColor: `rgb(${Math.floor(Math.random() * 250)},${Math.floor(Math.random() * 250)},${Math.floor(Math.random() * 250)})`,
+              backgroundColor: `rgba(${Math.floor(Math.random() * 250)},${Math.floor(Math.random() * 250)},${Math.floor(Math.random() * 250)},0.2)`,
             }}
           >
             <input
@@ -42,11 +45,27 @@ const Home = () => {
               value={navItem.name}
               checked={selected === navItem}
               onChange={() => setSelected(navItem)}
-              className="sr-only h-full w-full cursor-pointer"
+              onClick={() => (selected === navItem ? setSelected(null) : setSelected(navItem))}
+              className="sr-only cursor-pointer border"
             />
-            <p className="rotate-270 border bg-purple-500 text-4xl">{navItem.name}</p>
-            <p> {index === navList.indexOf(selected) && navItem.info}</p>{' '}
-            {/* Needs to be improved */}
+            <div className="flex flex-col items-center gap-1">
+              <p className="text-primary text-center text-2xl leading-none tracking-widest uppercase">
+                {navItem.name.split('').map((char: string, i: number) => (
+                  <span key={`${i}-${char}`} className="block">
+                    {char === ' ' ? <span className="opacity-0">·</span> : char}
+                  </span>
+                ))}
+              </p>
+            </div>
+            {selected === navItem && (
+              <div className="flex h-1/3 flex-col justify-between">
+                <p className="text-primary"> {navItem.info}</p>
+                <Link className="ml-auto" to={navItem.path}>
+                  <span className="bg-primary absolute h-12 w-12 animate-ping rounded-full opacity-30" />
+                  <ArrowRightIcon className="stroke-primary-light dark:stroke-primary-dark h-12 w-12" />
+                </Link>
+              </div>
+            )}
           </label>
         ))}
       </fieldset>
