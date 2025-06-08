f07f736a92e71218c77e3dbfb91e006130067a60
tomabadie <abadie.thomas@gmail.com>
2025-06-08 20:49:23 +0200

    Home responsive WIP - Done display transitions of infos

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -13,31 +13,24 @@ const Home = () => {
   const navList = language === 'en' ? navDataEn : navDataFr;
   const [selected, setSelected] = useState<NavigationItemProps | null>(null);
 
-  const getSizes = (index: number): string => {
-    const selectedIndex = selected ? navList.indexOf(selected) : -1;
-    if (index === selectedIndex) return 'w-[55%]';
-    /* if (Math.abs(index - selectedIndex) === 1) return 'basis-[20%]'; */
-    return 'w-[15%]';
-  };
-
   return (
-    <div className="flex flex-col items-center justify-around gap-4 border border-purple-500 px-2">
-      <h2 className="text-primary transition-theme border border-amber-400 text-5xl uppercase">
+    <div className="flex h-[calc(100vh-61px-37px)] flex-col items-center gap-4 border border-purple-500 px-2 md:h-fit md:justify-around">
+      <h2 className="text-primary transition-theme border border-amber-400 text-3xl uppercase sm:text-5xl">
         {language === 'en' ? 'Explore' : 'Exploration'}
       </h2>
-      <p className="">
+      <p className="text-primary transition-theme border border-blue-400 text-center">
         {language === 'en'
           ? 'A clear view into my developer journey'
           : `Une porte d'entrée vers mon univers de développeur.`}
       </p>
 
-      <fieldset className="flex h-[50dvh] w-[80vw] items-center justify-center gap-3 border border-green-500">
-        {navList.map((navItem, index) => (
+      <fieldset className="flex h-full flex-col items-center justify-center gap-3 border border-green-500 md:h-[60dvh] md:w-[80vw] md:flex-row">
+        {navList.map((navItem) => (
           <label
             key={navItem.id}
-            className={`flex h-full cursor-pointer items-center justify-around rounded-lg transition-all duration-300 ease-in-out ${getSizes(index)}`}
+            className="transition-theme bg-global-secondary border-primary flex h-full cursor-pointer items-center justify-around rounded-lg border transition-[heigth] duration-700 ease-in-out sm:transition-[width]"
             style={{
-              backgroundColor: `rgba(${Math.floor(Math.random() * 250)},${Math.floor(Math.random() * 250)},${Math.floor(Math.random() * 250)},0.2)`,
+              width: selected === navItem ? '55%' : '15%',
             }}
           >
             <input
@@ -45,7 +38,6 @@ const Home = () => {
               name="navItem"
               value={navItem.name}
               checked={selected === navItem}
-              onChange={() => setSelected(navItem)}
               onClick={() => (selected === navItem ? setSelected(null) : setSelected(navItem))}
               className="sr-only cursor-pointer border"
             />
@@ -53,7 +45,7 @@ const Home = () => {
               <IconWrapper variant={navItem.iconVariant} wrapperClassName="h-10 w-10">
                 {navItem.iconSvg}
               </IconWrapper>
-              <p className="text-primary text-center text-3xl leading-none tracking-widest uppercase">
+              <p className="text-primary text-center text-xl leading-none tracking-widest uppercase md:text-3xl">
                 {navItem.name.split('').map((char: string, i: number) => (
                   <span key={`${i}-${char}`} className="block">
                     {char === ' ' ? <span className="opacity-0">·</span> : char}
@@ -63,7 +55,7 @@ const Home = () => {
               <span className="opacity-0">·</span>
             </div>
             {selected === navItem && (
-              <div className="flex h-1/3 flex-col justify-between px-4">
+              <div className="animate-fade-in-delay flex h-1/3 flex-col justify-between px-4">
                 <p className="text-primary"> {navItem.info}</p>
                 <Link className="ml-auto" to={navItem.path}>
                   <span className="bg-primary absolute h-12 w-12 animate-ping rounded-full opacity-30" />
