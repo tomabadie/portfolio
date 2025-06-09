d49fe5d4eef8386f541aa21633281df276768e87
tomabadie <abadie.thomas@gmail.com>
2025-06-09 02:23:51 +0200

    Add hook to enable size transition - Home mobile version almost done

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,11 +7,13 @@ import { navDataFr } from '../../components/navigation/data/navData.fr';
 import IconWrapper from '../../components/ui/IconWrapper';
 import { ArrowRightIcon } from '../../components/ui/Icons';
 import { useLanguage } from '../../contexts/LanguageContext';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
 
 const Home = () => {
   const { language } = useLanguage();
   const navList = language === 'en' ? navDataEn : navDataFr;
   const [selected, setSelected] = useState<NavigationItemProps | null>(null);
+  const isDesktop = useMediaQuery('(min-width: 768px)');
 
   return (
     <div className="flex h-[calc(100vh-61px-37px)] flex-col items-center gap-4 border border-purple-500 px-2 md:h-fit md:justify-around">
@@ -24,47 +26,53 @@ const Home = () => {
           : `Une porte d'entrée vers mon univers de développeur.`}
       </p>
 
-      <fieldset className="flex h-full flex-col items-center justify-center gap-3 border border-green-500 md:h-[60dvh] md:w-[80vw] md:flex-row">
-        {navList.map((navItem) => (
-          <label
-            key={navItem.id}
-            className="transition-theme bg-global-secondary border-primary flex h-full cursor-pointer items-center justify-around rounded-lg border transition-[heigth] duration-700 ease-in-out sm:transition-[width]"
-            style={{
-              width: selected === navItem ? '55%' : '15%',
-            }}
-          >
-            <input
-              type="radio"
-              name="navItem"
-              value={navItem.name}
-              checked={selected === navItem}
-              onClick={() => (selected === navItem ? setSelected(null) : setSelected(navItem))}
-              className="sr-only cursor-pointer border"
-            />
-            <div className="flex h-full flex-col items-center justify-between gap-2 p-2">
-              <IconWrapper variant={navItem.iconVariant} wrapperClassName="h-10 w-10">
-                {navItem.iconSvg}
-              </IconWrapper>
-              <p className="text-primary text-center text-xl leading-none tracking-widest uppercase md:text-3xl">
-                {navItem.name.split('').map((char: string, i: number) => (
-                  <span key={`${i}-${char}`} className="block">
-                    {char === ' ' ? <span className="opacity-0">·</span> : char}
-                  </span>
-                ))}
-              </p>
-              <span className="opacity-0">·</span>
-            </div>
-            {selected === navItem && (
-              <div className="animate-fade-in-delay flex h-1/3 flex-col justify-between px-4">
-                <p className="text-primary"> {navItem.info}</p>
-                <Link className="ml-auto" to={navItem.path}>
-                  <span className="bg-primary absolute h-12 w-12 animate-ping rounded-full opacity-30" />
-                  <ArrowRightIcon className="stroke-primary-light dark:stroke-primary-dark h-12 w-12" />
-                </Link>
+      <fieldset className="flex h-full w-full flex-col items-center justify-center gap-3 border border-green-500 md:h-[60dvh] md:w-[80vw] md:flex-row">
+        {navList.map((navItem) => {
+          const isSelected = selected === navItem;
+          return (
+            <label
+              key={navItem.id}
+              className="transition-theme bg-global-secondary border-primary flex h-full cursor-pointer flex-col items-center justify-around rounded-lg border border-purple-600 transition-all duration-700 ease-in-out md:flex-row"
+              style={{
+                width: isDesktop ? (isSelected ? '55%' : '15%') : '100%',
+                height: !isDesktop ? (isSelected ? '55%' : '15%') : '100%',
+              }}
+            >
+              <input
+                type="radio"
+                name="navItem"
+                value={navItem.name}
+                checked={selected === navItem}
+                onClick={() => (selected === navItem ? setSelected(null) : setSelected(navItem))}
+                className="sr-only cursor-pointer border"
+              />
+              <div className="flex items-center justify-between gap-2 border border-amber-400 p-2 md:h-full md:flex-col">
+                <IconWrapper variant={navItem.iconVariant} wrapperClassName="h-10 w-10">
+                  {navItem.iconSvg}
+                </IconWrapper>
+                <p className="text-primary transition-theme w-52 text-center text-2xl uppercase md:w-auto md:text-3xl md:leading-none md:tracking-widest">
+                  {isDesktop &&
+                    navItem.name.split('').map((char: string, i: number) => (
+                      <span key={`${i}-${char}`} className="block">
+                        {char === ' ' ? <span className="opacity-0">·</span> : char}
+                      </span>
+                    ))}
+                  {!isDesktop && navItem.name}
+                </p>
+                <span className="opacity-0">·</span>
               </div>
-            )}
-          </label>
-        ))}
+              {selected === navItem && (
+                <div className="animate-fade-in-delay flex h-1/3 items-center justify-between px-4 md:flex-col">
+                  <p className="text-primary transition-theme"> {navItem.info}</p>
+                  <Link className="ml-auto" to={navItem.path}>
+                    <span className="bg-primary transition-theme absolute h-12 w-12 animate-ping rounded-full opacity-15" />
+                    <ArrowRightIcon className="stroke-primary-light dark:stroke-primary-dark h-12 w-12" />
+                  </Link>
+                </div>
+              )}
+            </label>
+          );
+        })}
       </fieldset>
     </div>
   );
