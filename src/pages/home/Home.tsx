646badd10e5d849c22bd35c576ef5588b6b137c8
tomabadie <abadie.thomas@gmail.com>
2025-06-09 13:44:02 +0200

    Last Home adjustments : Responsive - Style

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -16,23 +16,23 @@ const Home = () => {
   const isDesktop = useMediaQuery('(min-width: 768px)');
 
   return (
-    <div className="flex h-[calc(100vh-61px-37px)] flex-col items-center gap-4 border border-purple-500 px-2 md:h-fit md:justify-around">
-      <h2 className="text-primary transition-theme border border-amber-400 text-3xl uppercase sm:text-5xl">
+    <div className="flex h-[calc(100vh-61px-37px)] flex-col items-center gap-4 px-2 md:h-fit md:justify-around">
+      <h2 className="text-primary transition-theme border-primary border-b-2 px-10 pb-2 text-3xl font-semibold uppercase sm:text-4xl">
         {language === 'en' ? 'Explore' : 'Exploration'}
       </h2>
-      <p className="text-primary transition-theme border border-blue-400 text-center">
+      <p className="text-primary transition-theme text-center">
         {language === 'en'
           ? 'A clear view into my developer journey'
           : `Une porte d'entrée vers mon univers de développeur.`}
       </p>
 
-      <fieldset className="flex h-full w-full flex-col items-center justify-center gap-3 border border-green-500 md:h-[60dvh] md:w-[80vw] md:flex-row">
+      <fieldset className="flex h-full w-full flex-col items-center justify-center gap-3 sm:w-[80vw] md:h-[60dvh] md:flex-row">
         {navList.map((navItem) => {
           const isSelected = selected === navItem;
           return (
             <label
               key={navItem.id}
-              className="transition-theme bg-global-secondary border-primary flex h-full cursor-pointer flex-col items-center justify-around rounded-lg border border-purple-600 transition-all duration-700 ease-in-out md:flex-row"
+              className="transition-theme bg-global-secondary border-primary border-primary flex h-full cursor-pointer flex-col items-center justify-around rounded-lg border transition-all duration-700 ease-in-out md:flex-row"
               style={{
                 width: isDesktop ? (isSelected ? '55%' : '15%') : '100%',
                 height: !isDesktop ? (isSelected ? '55%' : '15%') : '100%',
@@ -46,7 +46,7 @@ const Home = () => {
                 onClick={() => (selected === navItem ? setSelected(null) : setSelected(navItem))}
                 className="sr-only cursor-pointer border"
               />
-              <div className="flex items-center justify-between gap-2 border border-amber-400 p-2 md:h-full md:flex-col">
+              <div className="flex items-center justify-between gap-2 p-2 md:h-full md:flex-col">
                 <IconWrapper variant={navItem.iconVariant} wrapperClassName="h-10 w-10">
                   {navItem.iconSvg}
                 </IconWrapper>
@@ -62,10 +62,10 @@ const Home = () => {
                 <span className="opacity-0">·</span>
               </div>
               {selected === navItem && (
-                <div className="animate-fade-in-delay flex h-1/3 items-center justify-between px-4 md:flex-col">
+                <div className="animate-fade-in-delay flex h-1/3 items-center justify-between gap-5 px-4 md:flex-col">
                   <p className="text-primary transition-theme"> {navItem.info}</p>
                   <Link className="ml-auto" to={navItem.path}>
-                    <span className="bg-primary transition-theme absolute h-12 w-12 animate-ping rounded-full opacity-15" />
+                    <span className="bg-primary transition-theme absolute h-12 w-12 animate-ping rounded-full opacity-20" />
                     <ArrowRightIcon className="stroke-primary-light dark:stroke-primary-dark h-12 w-12" />
                   </Link>
                 </div>
