f672dac6750eb34a879a15233cc07a6a451d65ff
tomabadie <abadie.thomas@gmail.com>
2025-06-14 00:29:02 +0200

    Change colors in Stacklist to improve contrast and visibility

--- src/components/ui/StackList.tsx
+++ src/components/ui/StackList.tsx
@@ -44,7 +44,7 @@ const StackList = ({ type, styleVariant, orientationVariant }: StackListProps) =
         type.content.map((item) => {
           return (
             <li key={item} className="group relative flex h-10 w-10 items-center justify-center">
-              <IconWrapper variant="basic" wrapperClassName="h-6 w-6 cursor-pointer text-secondary">
+              <IconWrapper variant="basic" wrapperClassName="h-6 w-6 cursor-pointer text-primary">
                 {skillsIcons[item.toLowerCase()]}
               </IconWrapper>
               <span className="border-primary bg-global-primary absolute top-full z-20 mt-1 scale-0 rounded-sm border px-2 py-1 text-xs opacity-0 shadow transition-all duration-200 group-hover:scale-100 group-hover:opacity-100 motion-reduce:transition-none">
@@ -63,7 +63,7 @@ const StackList = ({ type, styleVariant, orientationVariant }: StackListProps) =
               transition={{ duration: 0.3 }}
               className={`flex items-center justify-center flex-${orientationVariant} `}
             >
-              <span className="border-primary bg-global-primary transition-theme text-secondary rounded-sm border px-2 py-1 text-xs shadow motion-reduce:transition-none">
+              <span className="border-primary bg-global-primary transition-theme text-primary rounded-sm border px-2 py-1 text-xs shadow motion-reduce:transition-none">
                 {item}
               </span>
             </motion.li>
