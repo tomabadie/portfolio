16bf66ca08780f13ba01e24c9ca746f1bd530943
tomabadie <abadie.thomas@gmail.com>
2025-06-30 23:42:11 +0200

    Enhance form perf and display - WIP

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,29 +1,28 @@
 import emailjs from '@emailjs/browser';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import SocialGlobal from '../../components/social/SocialGlobal';
 import Toast from '../../components/ui/Toast';
 import { useLanguage } from '../../contexts/LanguageContext';
-import type { ContactFormDataProps } from './data/contactType';
 
 const Contact = () => {
   const { language } = useLanguage();
 
   const [toast, setToast] = useState<{ message: string; type: 'success' | 'error' } | null>(null);
 
-  const [formData, setFormData] = useState<ContactFormDataProps>({
+  /* const [formData, setFormData] = useState<ContactFormDataProps>({
     name: '',
     email: '',
     message: '',
-  });
+  }); */
 
   const nameMin = 2;
   const nameMax = 50;
   const messageMin = 20;
   const messageMax = 500;
 
-  const [isDisabled, setIsDisabled] = useState(true);
+  /* const [isDisabled, setIsDisabled] = useState(true); */
 
-  useEffect(() => {
+  /* useEffect(() => {
     const allValid =
       formData.name.length >= nameMin &&
       formData.name.length <= nameMax &&
@@ -31,24 +30,37 @@ const Contact = () => {
       formData.message.length >= messageMin &&
       formData.message.length <= messageMax;
     setIsDisabled(!allValid);
-  }, [formData]);
+  }, [formData]); */
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  /* const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = event.target;
     setFormData((prevForm) => ({
       ...prevForm,
       [name]: value,
     }));
-  };
+  }; */
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = async (formData: FormData) => {
+    /* e.preventDefault(); */
+    /* console.log('formData :');
+    console.log(formData); */
+    const formObj = Object.fromEntries(formData);
+    /*     const name = formData.get('name');
+    const email = formData.get('email');
+    const message = formData.get('message'); */
+    /* console.log('formObj :');
+    console.log(formObj); */
+
+    /* if (typeof name !== 'string' || typeof email !== 'string' || typeof message !== 'string') {
+      console.error('Invalid form data', { name, email, message });
+      return;
+    } */
 
     try {
       await emailjs.send(
         'service_vgkqpup',
         'template_9j8s85i',
-        { name: formData.name, email: formData.email, message: formData.message },
+        { name: formObj.name, email: formObj.email, message: formObj.message },
         {
           publicKey: '7uftDwFBfqwkFU4if',
         }
@@ -57,7 +69,7 @@ const Contact = () => {
         message: `${language === 'en' ? 'Message sent !' : 'Message envoyé !'}`,
         type: 'success',
       });
-      setFormData({ name: '', email: '', message: '' });
+      /* setFormData({ name: '', email: '', message: '' }); */
     } catch (err) {
       console.error(err);
       setToast({
@@ -82,7 +94,10 @@ const Contact = () => {
           <strong>abadie.thomas@gmail.com</strong>
         </p>
 
-        <form className="flex w-full flex-col gap-8 py-4 md:w-xl lg:w-3xl" onSubmit={handleSubmit}>
+        <form
+          className="group flex w-full flex-col gap-8 py-4 md:w-xl lg:w-3xl"
+          action={handleSubmit}
+        >
           {/* Name */}
           <label htmlFor="Name" className="relative">
             <input
@@ -93,10 +108,10 @@ const Contact = () => {
               aria-label="Name"
               placeholder=""
               required
-              value={formData.name}
               minLength={nameMin}
               maxLength={nameMax}
-              onChange={handleChange}
+              /* value={formData.name} */
+              /* onChange={handleChange} */
               autoComplete="name"
               className="peer border-primary transition-theme focus:ring-accent-primary-light dark:focus:ring-accent-primary-dark w-full rounded border px-2 py-2.5 user-valid:border-green-600/40 user-invalid:border-red-700/40 focus:ring-2 focus:outline-none motion-reduce:transition-none lg:w-2/3"
             />
@@ -108,7 +123,7 @@ const Contact = () => {
             </span>
             <p
               aria-live="polite"
-              className="hidden px-2 text-xs text-red-700 peer-[&:user-invalid]:block"
+              className="transition-theme px-2 text-xs text-red-700 opacity-0 peer-[&:user-invalid]:opacity-100"
             >
               {language === 'en'
                 ? 'Name must be between 2 and 50 characters'
@@ -126,8 +141,8 @@ const Contact = () => {
               /* pattern="^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$" */
               placeholder=""
               required
-              value={formData.email}
-              onChange={handleChange}
+              /* value={formData.email} */
+              /* onChange={handleChange} */
               autoComplete="email"
               className="peer border-primary transition-theme focus:ring-accent-primary-light dark:focus:ring-accent-primary-dark w-full rounded border px-2 py-2.5 user-valid:border-green-600/40 user-invalid:border-red-700/40 focus:ring-2 focus:outline-none motion-reduce:transition-none lg:w-2/3"
             />
@@ -139,7 +154,7 @@ const Contact = () => {
             </span>
             <p
               aria-live="polite"
-              className="hidden px-2 text-xs text-red-700 peer-[&:user-invalid]:block"
+              className="transition-theme px-2 text-xs text-red-700 opacity-0 peer-[&:user-invalid]:opacity-100"
             >
               {language === 'en' ? 'Please enter a valid email' : 'Veuillez entrer un email valide'}
             </p>
@@ -154,10 +169,10 @@ const Contact = () => {
               aria-label="Message"
               placeholder=""
               required
-              value={formData.message}
+              /* value={formData.message} */
               minLength={messageMin}
               maxLength={messageMax}
-              onChange={handleChange}
+              /* onChange={handleChange} */
               className="peer border-primary transition-theme focus:ring-accent-primary-light dark:focus:ring-accent-primary-dark w-full rounded border px-2 py-2.5 user-valid:border-green-600/40 user-invalid:border-red-700/40 focus:ring-2 focus:outline-none motion-reduce:transition-none"
               rows={7}
             />
@@ -167,10 +182,9 @@ const Contact = () => {
             >
               Message *
             </span>
-            <span className="px-2 text-xs">{formData.message.length}/500</span>
             <p
               aria-live="polite"
-              className="hidden px-2 text-xs text-red-700 peer-[&:user-invalid]:block"
+              className="transition-theme px-2 text-xs text-red-700 opacity-0 peer-[&:user-invalid]:opacity-100"
             >
               {language === 'en'
                 ? 'Message must be between 20 and 500 characters'
@@ -182,9 +196,9 @@ const Contact = () => {
           <button
             type="submit"
             value="Send"
-            data-cursor={isDisabled ? 'none' : 'hover'}
-            disabled={isDisabled}
-            className={`text-primary bg-global-primary border-primary ${!isDisabled && 'hover:bg-btn-primary-hover-light dark:hover:bg-btn-primary-hover-dark'} transition-theme ml-auto block w-30 rounded-lg border py-2 text-lg font-semibold ${isDisabled && 'cursor-not-allowed opacity-50 ring ring-red-400'}`}
+            data-cursor="hover"
+            /* disabled={isDisabled} */
+            className="text-primary bg-global-primary border-primary hover:bg-btn-primary-hover-light dark:hover:bg-btn-primary-hover-dark transition-theme ml-auto block w-30 rounded-lg border py-2 text-lg font-semibold group-invalid:opacity-20"
           >
             {language === 'en' ? 'Send' : 'Envoyer'}
           </button>
